3eaf80e3665aa03e68c80a6203729744b89ebe9d
lovelumine <shendekoudai@lovelumine.com>
2024-09-05 15:09:58 +0800

    初始化项目

---
+++ Mentor-Dual-Selection-System/src/main/java/mentordualselectionsystem/MentorDualSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package mentordualselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorDualSelectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorDualSelectionSystemApplication.class, args);
+	}
+
+}
