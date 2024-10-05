f37ea64eb05f2a3fb752ec6fbfa0cc58592603e6
lovelumine <shendekoudai@lovelumine.com>
2024-10-06 04:21:28 +0800

    师生关系（学生）增加滚动条

--- Mentor-Dual-Selection-System-Frontend/src/components/Home/Relations/Student/StudentToTeacherComp.vue
+++ Mentor-Dual-Selection-System-Frontend/src/components/Home/Relations/Student/StudentToTeacherComp.vue
@@ -1,78 +1,88 @@
 <script setup lang="ts">
-
-import {onMounted, ref, watch} from "vue";
-import {useUserInfoStore} from "@/stores/user/UserBasicInformation";
-import {Right} from "@element-plus/icons-vue";
-import {http} from "@/utils/http";
+import { onMounted, ref, watch } from "vue";
+import { useUserInfoStore } from "@/stores/user/UserBasicInformation";
+import { Right } from "@element-plus/icons-vue";
+import { http } from "@/utils/http";
 const userStore = useUserInfoStore();
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 const router = useRouter();
 
 const userInfoComp = ref({
-  fullName: '',
-  email: '',
-  avatarUrl: '',
-  role: ''
+  fullName: "",
+  email: "",
+  avatarUrl: "",
+  role: "",
 });
 const targetTeacher = ref({
-  fullName: '',
-  email: '',
-  avatarUrl: ''
+  fullName: "",
+  email: "",
+  avatarUrl: "",
 });
 const errorShow = ref(0);
-
 
 onMounted(() => {
   userStore.fetchUserInfo();
   http({
-    url: '/application/mentor-student-relations',
-    method: 'GET',
+    url: "/application/mentor-student-relations",
+    method: "GET",
     headers: {
-      'Content-Type': '*/*',
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
-    }
-  }).then(res => {
-    if (res.data.code === 200) {
-      targetTeacher.value = {...res.data.data[0].mentor};
-    } else{
-      alert('获取关键信息失败，请检查网络和登录状态！');
-    }
-  }).catch(err => {
-    console.error(err);
-    if (err.request.status === 404) errorShow.value = 404;
-    else if (err.request.status === 500) errorShow.value = 500;
+      "Content-Type": "*/*",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
   })
-})
+    .then((res) => {
+      if (res.data.code === 200) {
+        targetTeacher.value = { ...res.data.data[0].mentor };
+      } else {
+        alert("获取关键信息失败，请检查网络和登录状态！");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.request.status === 404) errorShow.value = 404;
+      else if (err.request.status === 500) errorShow.value = 500;
+    });
+});
 
-watch(() => userStore.userInfo, (newValue) => {
-  userInfoComp.value = {...newValue};
-})
+watch(
+  () => userStore.userInfo,
+  (newValue) => {
+    userInfoComp.value = { ...newValue };
+  }
+);
 </script>
 
 <template>
-  <div class="student_to_teacher_box" v-if="errorShow === 0">
-    <h3>您的目标导师</h3>
-    <div class="item_box">
-      <div class="item">
-        <img class="avatar" :src="userInfoComp.avatarUrl" alt="avatar">
-        <div>学生姓名：{{userInfoComp.fullName}}</div>
-        <div>学生邮箱：{{userInfoComp.email}}</div>
+  <div class="container">
+    <div class="student_to_teacher_box" v-if="errorShow === 0">
+      <h3>您的目标导师</h3>
+      <div class="item_box">
+        <div class="item">
+          <img class="avatar" :src="userInfoComp.avatarUrl" alt="avatar" />
+          <div>学生姓名：{{ userInfoComp.fullName }}</div>
+          <div>学生邮箱：{{ userInfoComp.email }}</div>
+        </div>
+      </div>
+      <el-icon :size="48" :color="'#005826'"><Right /></el-icon>
+      <div class="item_box">
+        <div class="item">
+          <div>导师姓名：{{ targetTeacher.fullName }}</div>
+          <div>导师邮箱：{{ targetTeacher.email }}</div>
+        </div>
       </div>
     </div>
-    <el-icon :size="48" :color="'#005826'"><Right/></el-icon>
-    <div class="item_box">
-      <div class="item">
-        <div>导师姓名：{{targetTeacher.fullName}}</div>
-        <div>导师邮箱：{{targetTeacher.email}}</div>
-      </div>
+    <div class="student_to_teacher_box" v-if="errorShow === 404">
+      <h3>您还未选择导师</h3>
     </div>
-  </div>
-  <div class="student_to_teacher_box" v-if="errorShow === 404">
-    <h3>您还未选择导师</h3>
   </div>
 </template>
 
 <style scoped lang="sass">
+/* 页面整体容器 */
+.container
+  margin-top: 60px  /* 向下移动60px */
+  overflow-y: auto   /* 当内容超出页面范围时，显示滚动条 */
+
 .student_to_teacher_box
   display: flex
   align-items: center
