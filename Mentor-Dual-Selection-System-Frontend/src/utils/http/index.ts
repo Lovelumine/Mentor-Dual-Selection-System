97e99b61ebb34f50843444eb716aff25aca6e679
Yasenbaka <1260352143@qq.com>
2024-10-18 18:37:53 +0800

    30秒延迟等待

--- Mentor-Dual-Selection-System-Frontend/src/utils/http/index.ts
+++ Mentor-Dual-Selection-System-Frontend/src/utils/http/index.ts
@@ -2,24 +2,24 @@ import axios from 'axios';
 
 export const http = axios.create({
     baseURL: '/api',
-    timeout: 5000,
+    timeout: 30000,
     withCredentials: true
 })
 
 export const httpAdmin = axios.create({
     baseURL: '/admin',
-    timeout: 5000,
+    timeout: 30000,
     withCredentials: true
 })
 
 export const httpTeacher = axios.create({
     baseURL: '/teacher',
-    timeout: 5000,
+    timeout: 30000,
     withCredentials: true
 })
 
 export const httpStudent = axios.create({
     baseURL: '/student',
-    timeout: 5000,
+    timeout: 30000,
     withCredentials: true
 })
